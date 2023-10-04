38132dd5358ac51af358addd4730689b895bfea9
simonoff <hellosimonoff@gmail.com>
2023-10-04 11:55:21 +0200

    updated footer markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,18 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-200 bg-white py-8 dark:border-gray-700 dark:bg-gray-800">
-      <div className="mx-auto max-w-screen-xl px-4 text-center font-mono text-sm">
-        copy and redesign of{" "}
-        <a href="https://inet.se" className="hover:text-blue-400">
-          www.inet.se
-        </a>
+    <footer className="border-t border-gray-200 bg-white py-12 dark:border-gray-700 dark:bg-gray-800">
+      <div className="mx-auto max-w-screen-xl px-4 text-center font-mono">
+        <p>
+          redesign of{" "}
+          <a href="https://inet.se" className="link">
+            www.inet.se
+          </a>{" "}
+          by{" "}
+          <a href="https://simonoff.se" className="link">
+            simonoff
+          </a>
+        </p>
+        <p className="mt-1 text-xs opacity-75">using React, TypeScript & Tailwind CSS</p>
       </div>
     </footer>
   );
